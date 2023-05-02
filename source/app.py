09def61bd3f5ab61d7930d99320ddd56851810ed
mirvoda_av <mirvoda_av@octonica.com>
2023-05-02 17:46:57 +0500

    Typing at app.py

--- source/app.py
+++ source/app.py
@@ -1,5 +1,6 @@
-from pyrogram import Client, filters
-from pyrogram.types import CallbackQuery
+from pyrogram import filters
+from pyrogram.client import Client
+from pyrogram.types import CallbackQuery, Message
 from pyrogram.handlers.callback_query_handler import CallbackQueryHandler
 from get_secrets import read_secrets
 from txt2img import txt2img
@@ -11,30 +12,33 @@ from dependencies import DependencyContainer
 
 from app_reply import GREETINGS, HELP
 
+START_IMAGE = "./static/startImg.png"
+APP_NAME = "MidjourneyDiffusion"
+
 
 def create_app(deps: DependencyContainer) -> Client:
     secrets = read_secrets()
     app = Client(
-        "MidjourneyDiffusion",
+        APP_NAME,
         api_id=secrets["API_ID"],
         api_hash=secrets["API_HASH"],
         bot_token=secrets["TOKEN"],
     )
 
     @app.on_message(filters.command(["start"], prefixes=["/", "!"]))
-    async def start(client, message):
-        start_image = "./static/startImg.png"
+    async def start(client: Client, message: Message):
+        start_image = START_IMAGE
         await message.reply_photo(
             photo=start_image,
             caption=GREETINGS,
         )
 
     @app.on_message(filters.command(["imagine"]))
-    def imagine(client, message):
+    async def imagine(client: Client, message: Message):
         txt2img(client, message, deps)
 
     @app.on_message(filters.command(["help"]))
-    async def help(client, message):
+    async def help(client: Client, message: Message):
         await message.reply_text(HELP)
 
     async def regenerate_process(client: Client, call: CallbackQuery):
